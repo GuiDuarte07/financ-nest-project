d7dbbde4d3ca9276e20f1a03db0a5d6cf372659c
Guilherme Duarte <guilduarte07@gmail.com>
2023-08-21 17:19:49 -0300

    fix(migration): apagando e refazendo o db

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello :D This is a Nest aplication made by me, Guilherme Duarte.\nWelcome!';
   }
 }
