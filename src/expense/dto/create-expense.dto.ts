2755095ad9331af0cc1858e4f594a2953f0d69c6
Guilherme Duarte <guilduarte07@gmail.com>
2023-08-22 14:07:02 -0300

    feat(expense): `create-expense.DTO` criado

--- src/expense/dto/create-expense.dto.ts
+++ src/expense/dto/create-expense.dto.ts
@@ -1,1 +1,24 @@
-export class CreateExpenseDto {}
+import {
+  IsDate,
+  IsNumber,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateExpenseDto {
+  @MaxLength(120)
+  @MinLength(5)
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  value: number;
+
+  @IsDate()
+  purchaseDate?: Date;
+
+  @IsUUID()
+  accountId: string;
+}
