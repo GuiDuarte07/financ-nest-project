7165a8e5c8343f9a9909e7edbe3686f9c1f9813b
Guilherme Duarte <guilduarte07@gmail.com>
2023-08-22 22:03:11 -0300

    feat(credit-card-expense): `DTO`

--- src/credit-card-expense/dto/create-credit-card-expense.dto.ts
+++ src/credit-card-expense/dto/create-credit-card-expense.dto.ts
@@ -1,1 +1,39 @@
-export class CreateCreditCardExpenseDto {}
+import {
+  IsDateString,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+  Max,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateCreditCardExpenseDto {
+  @MaxLength(120)
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  justForRecord?: boolean;
+
+  @IsNumber()
+  @IsPositive()
+  value: number;
+
+  @IsInt()
+  @Max(25)
+  installment: number; //parcelas
+
+  @IsOptional()
+  @IsDateString()
+  purchaseDate?: Date;
+
+  @IsOptional()
+  @IsDateString()
+  invoiceDate?: Date;
+
+  @IsUUID()
+  creditCardId: string;
+}
