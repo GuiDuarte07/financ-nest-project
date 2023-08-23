c73cb3acdbf9c707e578dfaacf113099f1e38613
Guilherme Duarte <guilduarte07@gmail.com>
2023-08-22 22:25:15 -0300

    feat(credit-card-expense): `controller` implementado

--- src/credit-card-expense/credit-card-expense.controller.ts
+++ src/credit-card-expense/credit-card-expense.controller.ts
@@ -10,6 +10,9 @@ import {
 import { CreditCardExpenseService } from './credit-card-expense.service';
 import { CreateCreditCardExpenseDto } from './dto/create-credit-card-expense.dto';
 import { UpdateCreditCardExpenseDto } from './dto/update-credit-card-expense.dto';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { IUserPayload } from 'src/user/interfaces/user';
+import { FindCreditCardExpenseDTO } from './dto/find-credit-card-expense.dto';
 
 @Controller('credit-card-expense')
 export class CreditCardExpenseController {
@@ -18,30 +21,49 @@ export class CreditCardExpenseController {
   ) {}
 
   @Post()
-  create(@Body() createCreditCardExpenseDto: CreateCreditCardExpenseDto) {
-    return this.creditCardExpenseService.create(createCreditCardExpenseDto);
+  create(
+    @Body() createCreditCardExpenseDto: CreateCreditCardExpenseDto,
+    @CurrentUser() user: IUserPayload,
+  ) {
+    return this.creditCardExpenseService.create(
+      createCreditCardExpenseDto,
+      user.id,
+    );
   }
 
   @Get()
-  findAll() {
-    return this.creditCardExpenseService.findAll();
+  findAll(
+    @Body() findCreditCardExpenseDTO: FindCreditCardExpenseDTO,
+    @CurrentUser() user: IUserPayload,
+  ) {
+    return this.creditCardExpenseService.findAll(
+      user.id,
+      findCreditCardExpenseDTO.startDate,
+      findCreditCardExpenseDTO.endDate,
+      findCreditCardExpenseDTO.creditCardId,
+    );
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.creditCardExpenseService.findOne(id);
+  findOne(@Param('id') id: string, @CurrentUser() user: IUserPayload) {
+    return this.creditCardExpenseService.findOne(id, user.id);
   }
 
   @Patch(':id')
   update(
     @Param('id') id: string,
     @Body() updateCreditCardExpenseDto: UpdateCreditCardExpenseDto,
+    @CurrentUser() user: IUserPayload,
   ) {
-    return this.creditCardExpenseService.update(id, updateCreditCardExpenseDto);
+    return this.creditCardExpenseService.update(
+      id,
+      updateCreditCardExpenseDto,
+      user.id,
+    );
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.creditCardExpenseService.remove(id);
+  remove(@Param('id') id: string, @CurrentUser() user: IUserPayload) {
+    return this.creditCardExpenseService.remove(id, user.id);
   }
 }
