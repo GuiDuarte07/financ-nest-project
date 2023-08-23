6b1aa6e1ab1597de92ab76e8cdca131a1434fc93
Guilherme Duarte <guilduarte07@gmail.com>
2023-08-22 22:28:12 -0300

    chore(credit-card-expense): import `PrismaService`

--- src/credit-card-expense/credit-card-expense.module.ts
+++ src/credit-card-expense/credit-card-expense.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CreditCardExpenseService } from './credit-card-expense.service';
 import { CreditCardExpenseController } from './credit-card-expense.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaService],
   controllers: [CreditCardExpenseController],
   providers: [CreditCardExpenseService],
 })
