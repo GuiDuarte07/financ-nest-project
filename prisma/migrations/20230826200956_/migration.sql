2ad46f914420a2fb41282ae7c127d3c3d1884729
Guilherme Duarte <guilduarte07@gmail.com>
2023-08-26 17:10:19 -0300

    fix(prisma): limpando migrations

---
+++ prisma/migrations/20230826200956_/migration.sql
@@ -0,0 +1,169 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "emailConfirmed" BOOLEAN DEFAULT false,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "balance" REAL NOT NULL,
+    "initialBalance" REAL NOT NULL,
+    "description" TEXT NOT NULL,
+    "bank" TEXT NOT NULL,
+    "color" TEXT,
+    "type" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "CreditCard" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "limit" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "cardBrand" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "closeDay" INTEGER NOT NULL,
+    "expirationDay" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "CreditCard_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "CreditCard_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "icon" TEXT,
+    "color" TEXT,
+    "type" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "description" TEXT NOT NULL,
+    "value" REAL NOT NULL,
+    "purchaseDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "justForRecord" BOOLEAN NOT NULL DEFAULT false,
+    "recorrenceId" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    CONSTRAINT "Expense_recorrenceId_fkey" FOREIGN KEY ("recorrenceId") REFERENCES "RecorrenceBillsProcess" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Expense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Expense_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Expense_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "CreditCardExpenseGenerator" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "justForRecord" BOOLEAN NOT NULL DEFAULT false,
+    "totalValue" REAL NOT NULL,
+    "earlyPaymentDiscount" REAL NOT NULL DEFAULT 0,
+    "installment" INTEGER NOT NULL,
+    "purchaseDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "invoiceDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "paymentEndDate" DATETIME NOT NULL,
+    "resolved" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "creditCardId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "CreditCardExpenseGenerator_creditCardId_fkey" FOREIGN KEY ("creditCardId") REFERENCES "CreditCard" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "CreditCardExpenseGenerator_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "CreditCardExpense" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "justForRecord" BOOLEAN NOT NULL DEFAULT false,
+    "value" REAL NOT NULL,
+    "paidOut" BOOLEAN NOT NULL DEFAULT false,
+    "anticipated" BOOLEAN NOT NULL DEFAULT false,
+    "invoiceDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "installmentNumber" INTEGER NOT NULL,
+    "totalInstallmentNumber" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "creditCardExpenseGeneratorId" TEXT NOT NULL,
+    "creditCardId" TEXT,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "CreditCardExpense_creditCardExpenseGeneratorId_fkey" FOREIGN KEY ("creditCardExpenseGeneratorId") REFERENCES "CreditCardExpenseGenerator" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "CreditCardExpense_creditCardId_fkey" FOREIGN KEY ("creditCardId") REFERENCES "CreditCard" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "CreditCardExpense_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Income" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" REAL NOT NULL,
+    "receivedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "description" TEXT,
+    "justForRecord" BOOLEAN NOT NULL DEFAULT false,
+    "recorrenceId" TEXT,
+    "categoryId" TEXT NOT NULL,
+    "accountId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Income_recorrenceId_fkey" FOREIGN KEY ("recorrenceId") REFERENCES "RecorrenceBillsProcess" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Income_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Income_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Income_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "CreditCardRecorrencePurchase" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "dayOfPayment" INTEGER NOT NULL,
+    "value" REAL NOT NULL,
+    "justForRecord" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "creditCardId" TEXT,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "CreditCardRecorrencePurchase_creditCardId_fkey" FOREIGN KEY ("creditCardId") REFERENCES "CreditCard" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "CreditCardRecorrencePurchase_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "RecorrenceBillsProcess" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "value" REAL NOT NULL,
+    "justForRecord" BOOLEAN NOT NULL DEFAULT false,
+    "stopCount" BOOLEAN NOT NULL DEFAULT false,
+    "paymentRecurrence" TEXT NOT NULL,
+    "monthlyDayOfPayment" INTEGER,
+    "weeklyDay" TEXT,
+    "countNumber" INTEGER NOT NULL DEFAULT 0,
+    "rememberDayOfPayment" BOOLEAN NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "startDate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
